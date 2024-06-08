93f0ccb6bd5b70ea36123c9f4d65bed45fd61a24
train106 <77904415+train106@users.noreply.github.com>
2024-06-08 13:44:06 -0600

    JS changes to Debussle

---
+++ assets/js/debussle.js
@@ -0,0 +1,30 @@
+fetch('orciety.github.io/assets/json/composers.json')
+  .then(response => response.json())
+  .then(jsonResponse => console.log(jsonResponse))     
+   // outputs a javascript object from the parsed json
+
+function drawBox(container, row, col, letter = "") {
+    const box = document.createElement("div");
+    box.className = "box";
+    box.id = `box${row}${col}`;
+    box.textContent = letter;
+
+    container.appendChild(box);
+}
+
+function drawGrid(container) {
+    const grid = document.createElement("div");
+    grid.className = "grid";
+
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 5; j++) {
+            drawBox(grid, i, j);
+        }
+    }
+}
+
+function startup() {
+
+}
+
+startup();
