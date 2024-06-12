920cd3b2c93a2150f0e41650880a65c71202a6e8
train106 <77904415+train106@users.noreply.github.com>
2024-06-11 18:24:14 -0600

    Debussle additions

--- assets/js/debussle.js
+++ assets/js/debussle.js
@@ -1,7 +1,22 @@
-fetch('assets/json/composers.json')
-  .then(response => response.json())
-  .then(jsonResponse => console.log(jsonResponse))     
-   // outputs a javascript object from the parsed json
+const dictionary = ["liszt", "bizet", "piano", "audio", "sound", "music", "forte"];
+
+const state = {
+    secret: disctionary[Math.floor(Math.random * dictionary.length)],
+    grid: Array(6)
+        .fill()
+        .map(() => Array(5).fill("")),
+    currentRow: 0,
+    currentCol: 0,
+};
+
+function updateGrid() {
+    for (let i = 0; i < state.grid.length; i++) {
+        for (let j = 0; j < state.grid[i].length; j++) {
+            const box = document.getElementById(`box${i}${j}`);
+            box.textContent = state.grid[i][j];
+        }
+    }
+}
 
 function drawBox(container, row, col, letter = "") {
     const box = document.createElement("div");
@@ -21,10 +36,92 @@ function drawGrid(container) {
             drawBox(grid, i, j);
         }
     }
+
+    container.appendChild(grid);
+}
+
+function registerKeyboardEvents() {
+    document.body.onkeydown = (e) => {
+        const key = e.key;
+        if (key === "Enter") {
+            if (state.currentCol === 5) {
+                const word = getCurrentWord();
+                if (isWordValid(word)) {
+                    revealWord(word);
+                    state.currentRow++;
+                    state.currentCol = 0;
+                } else {
+                    alert("I think this is not a real word cuh");
+                }
+            }
+        }
+        if (key === "Backspace") {
+            removeLetter();
+        }
+        if (isLetter(key)) {
+            addLetter(key);
+        }
+        updateGrid();
+    }
+}
+
+function getCurrentWord() {
+    return state.grid[state.currentRow].reduce((prev, curr) => prev + curr);
+}
+
+function isWordValid(word) {
+    return disctionary.includes(word);
+}
+
+function revealWord(guess) {
+    const row = state.currentRow;
+
+    for (let i = 0; i < 5; i++) {
+        const box = document.getElementById(`box${row}${i}`);
+        const letter = box.textContent;
+
+        if (letter === state.secret[i]) {
+            box.classList.add("right");
+        } else if (state.secret.includes(letter)) {
+            box.classList.add("wrong");
+        } else {
+            box.classList.add("empty");
+        }
+    }
+
+    const isWinner = state.secret === guess;
+    const isGameOver = state.currentRow === 5;
+
+    if (isWinner) {
+        alert("You won cuh");
+    } else if (isGameOver) {
+        alert(`You lost! The correct term was ${state.secret}.`);
+    }
+}
+
+function isLetter(key) {
+    return key.length == 1 && key.match(/[a-z]/i);
+}
+
+function addLetter(letter) {
+    if (state.currentCol === 5) return;
+    state.grid[state.currentRow][state.currentCol] = letter;
+    state.currentCol++;
+}
+
+function removeLetter() {
+    if (state.currentCol === 0) return;
+    state.grid[state.currentRow][state.currentCol - 1] = "";
+    state.currentCol--;
 }
 
 function startup() {
+    const game = document.getElementById("game");
+    drawGrid(game);
 
+    registerKeyboardEvents();
+
+    alert(state.secret);
 }
 
 startup();
